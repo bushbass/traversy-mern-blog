c53bef929eaaea2b7ec8619b0cfd3483cd954f01
Alex Nielsen <alex@bigal.org>
2023-10-20 12:30:31 -0400

    start at 'Logout Route'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 import asyncHandler from 'express-async-handler';
 
@@ -6,7 +7,22 @@ import asyncHandler from 'express-async-handler';
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send('auth user');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
 // @desc    Register a new user
@@ -29,6 +45,7 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({
       _id: user._id,
       name: user.name,
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,14 @@ import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000; // Run under this line...
 
 connectDB();
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRoutes);
