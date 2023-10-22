68d1de591ddacd1bf120bdc88c6649963d915b76
Alex Nielsen <alex@bigal.org>
2023-10-22 14:33:33 -0400

    start at register screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
       <Header />
       <Container className="my-2">
-        <HomeScreen />
+        <Outlet />
       </Container>
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} /> {/* Add this line */}
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
